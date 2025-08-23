vfazio/libgpiod

commit 8f62e6c45355588621ca5916cfab6d1d8e787a77

bindings: python: loosen type requirements in public API
Previously, `Chip.request_lines` and `LineRequest.reconfigure_lines`
were typed to accept a config argument that was either an int, a str,
or a tuple[int | str].

This had two downsides, namely:
* The tuple was typed as having only a single element and not a variable
  number of elements. The examples and test suite relied on a variable
  length tuple.
* The tuple type itself was overly restictive. The function
  implementations had no requirement that the value be a tuple, only
  that it was iterable if it was not a str or an int.

Now, these functions accept an Iterable[int | str] instead of tuples to
offer greater flexibility to callers.

This change does not break compatibility for existing users.

Closes: https://github.com/brgl/libgpiod/issues/102
Signed-off-by: Vincent Fazio <vfazio@xes-inc.com>
Link: https://lore.kernel.org/r/20241114145116.2123714-4-vfazio@xes-inc.com
Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

bindings/python/gpiod/chip.py
@@ -13,6 +13,7 @@ from .line_request import LineRequest
 from .line_settings import LineSettings, _line_settings_to_ext
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable
     from datetime import timedelta
 
     from .chip_info import ChipInfo
@@ -225,7 +226,9 @@ class Chip:
 
     def request_lines(
         self,
-        config: dict[tuple[Union[int, str]], Optional[LineSettings]],
+        config: dict[
+            Union[Iterable[Union[int, str]], int, str], Optional[LineSettings]
+        ],
         consumer: Optional[str] = None,
         event_buffer_size: Optional[int] = None,
         output_values: Optional[dict[Union[int, str], Value]] = None,

bindings/python/gpiod/line_request.py
@@ -148,7 +148,10 @@ class LineRequest:
         self._req.set_values(mapped)
 
     def reconfigure_lines(
-        self, config: dict[tuple[Union[int, str]], LineSettings]
+        self,
+        config: dict[
+            Union[Iterable[Union[int, str]], int, str], Optional[LineSettings]
+        ],
     ) -> None:
         """
         Reconfigure requested lines.

commit 9783a280daee2af9305a7d9a4ee7a74bd4a0e17c

bindings: python: parse non-tuple Iterable keys
When `chip.request_lines` was modified to allow `Iterable`s instead of
only `tuple`s, the code that checked for duplicate line entries in the
`config` argument was not updated to account for the expanded types.

If the `config` argument had a key that was not a `tuple`, `str`, or
`int`, a `TypeError` would be raised when resolving the line offset.

Refactor the code the resolves IDs to offsets into a separate function
to make the logic a bit clearer and to account for the widened types.

Fixes: 8f62e6c45355 ("bindings: python: loosen type requirements in public API")
Closes: https://github.com/brgl/libgpiod/issues/148
Signed-off-by: Vincent Fazio <vfazio@gmail.com>

bindings/python/gpiod/chip.py
@@ -236,6 +236,20 @@ class Chip:
         self._check_closed()
         return cast(_ext.Chip, self._chip).read_info_event()
 
+    def _resolve_config_keys_to_offsets(
+        self,
+        config_keys: Iterable[Union[Iterable[Union[int, str]], int, str]],
+    ) -> list[int]:
+        offsets: list[int] = list()
+        for key in config_keys:
+            # perform strict int/str check since str is also Iterable
+            if isinstance(key, (int, str)):
+                offsets.append(self.line_offset_from_id(key))
+            else:  # key is an iterable with multiple IDs to resolve
+                for item in key:
+                    offsets.append(self.line_offset_from_id(item))
+        return offsets
+
     def request_lines(
         self,
         config: dict[
@@ -271,14 +285,7 @@ class Chip:
 
         # Sanitize lines - don't allow offset repetitions or offset-name conflicts.
         for offset, count in Counter(
-            [
-                self.line_offset_from_id(line)
-                for line in (
-                    lambda t: [
-                        j for i in (t) for j in (i if isinstance(i, tuple) else (i,))
-                    ]
-                )(tuple(config.keys()))
-            ]
+            self._resolve_config_keys_to_offsets(config_keys=config.keys())
         ).items():
             if count != 1:
                 raise ValueError(

bindings/python/tests/tests_line_request.py
@@ -101,6 +101,10 @@ class ChipLineRequestWorks(TestCase):
         with self.chip.request_lines(config={(4): None}) as req:
             self.assertEqual(req.offsets, [4])
 
+    def test_request_single_offset_as_frozenset(self) -> None:
+        with self.chip.request_lines(config={frozenset([4]): None}) as req:
+            self.assertEqual(req.offsets, [4])
+
     def test_request_by_name(self) -> None:
         with self.chip.request_lines(config={(1, 2, "foo", "bar"): None}) as req:
             self.assertEqual(req.offsets, [1, 2, 5, 7])