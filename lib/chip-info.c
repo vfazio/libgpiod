vfazio/libgpiod

commit c040d4e15e5d4190895da5427db0a52b7457c12e

core: fix 1-byte buffer over-read bugs in gpiod_chip_info_from_uapi()
The copy in gpiod_chip_info_from_uapi() uses sizeof(info->name) = 33 as
the strncpy length while it should read at most GPIO_MAX_NAME_SIZE = 32.
Same for info->label. 33 is the size of the target buffer where we
allocate an additional byte for the NULL-terminator. Limit the number of
read bytes to the correct value.

Fixes: b7ba732e6a93 ("treewide: libgpiod v2 implementation")
Reported-by: 4fqr <4fqr@proton.me>
Tested-by: Vincent Fazio <vfazio@xes-inc.com>
Reviewed-by: Vincent Fazio <vfazio@xes-inc.com>
Link: https://patch.msgid.link/20260407-treewide-fixes-v1-3-66c9744a56a3@oss.qualcomm.com
Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>

lib/chip-info.c
@@ -57,7 +57,7 @@ gpiod_chip_info_from_uapi(struct gpiochip_info *uapi_info)
 	 * GPIO device must have a name - don't bother checking this field. In
 	 * the worst case (would have to be a weird kernel bug) it'll be empty.
 	 */
-	strncpy(info->name, uapi_info->name, sizeof(info->name));
+	strncpy(info->name, uapi_info->name, GPIO_MAX_NAME_SIZE);
 
 	/*
 	 * The kernel sets the label of a GPIO device to "unknown" if it
@@ -65,9 +65,9 @@ gpiod_chip_info_from_uapi(struct gpiochip_info *uapi_info)
 	 * we got an empty string, do the same.
 	 */
 	if (uapi_info->label[0] == '\0')
-		strncpy(info->label, "unknown", sizeof(info->label));
+		strncpy(info->label, "unknown", GPIO_MAX_NAME_SIZE);
 	else
-		strncpy(info->label, uapi_info->label, sizeof(info->label));
+		strncpy(info->label, uapi_info->label, GPIO_MAX_NAME_SIZE);
 
 	return info;
 }

commit 9face03bad0d050e3d7bf0461c716c15bd67c197

core: add explicit NUL-termination after strncpy() calls
Both gpiod_chip_info_from_uapi() and gpiod_line_info_from_uapi() copy
kernel UAPI name strings with strncpy() into buffers that are one byte
larger than GPIO_MAX_NAME_SIZE. If the source string is exactly
GPIO_MAX_NAME_SIZE bytes long strncpy() will not append a NUL, so the
strings are only guaranteed to be terminated because the structs are
zeroed with memset() beforehand.

Make the termination unconditional and independent of the zeroing by
writing the NUL byte explicitly after each strncpy() call.

Fixes: c040d4e15e5d ("core: fix 1-byte buffer over-read bugs in gpiod_chip_info_from_uapi()")
Fixes: 5533f277aa28 ("core: fix line-info strings termination")
Assisted-by: Claude Sonnet 4.6
Acked-by: Vincent Fazio <vfazio@xes-inc.com>
Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>

lib/chip-info.c
@@ -58,6 +58,7 @@ gpiod_chip_info_from_uapi(struct gpiochip_info *uapi_info)
 	 * the worst case (would have to be a weird kernel bug) it'll be empty.
 	 */
 	strncpy(info->name, uapi_info->name, GPIO_MAX_NAME_SIZE);
+	info->name[GPIO_MAX_NAME_SIZE] = '\0';
 
 	/*
 	 * The kernel sets the label of a GPIO device to "unknown" if it
@@ -68,6 +69,7 @@ gpiod_chip_info_from_uapi(struct gpiochip_info *uapi_info)
 		strncpy(info->label, "unknown", GPIO_MAX_NAME_SIZE);
 	else
 		strncpy(info->label, uapi_info->label, GPIO_MAX_NAME_SIZE);
+	info->label[GPIO_MAX_NAME_SIZE] = '\0';
 
 	return info;
 }

lib/line-info.c
@@ -149,9 +149,11 @@ gpiod_line_info_from_uapi(struct gpio_v2_line_info *uapi_info)
 
 	info->offset = uapi_info->offset;
 	strncpy(info->name, uapi_info->name, GPIO_MAX_NAME_SIZE);
+	info->name[GPIO_MAX_NAME_SIZE] = '\0';
 
 	info->used = !!(uapi_info->flags & GPIO_V2_LINE_FLAG_USED);
 	strncpy(info->consumer, uapi_info->consumer, GPIO_MAX_NAME_SIZE);
+	info->consumer[GPIO_MAX_NAME_SIZE] = '\0';
 
 	if (uapi_info->flags & GPIO_V2_LINE_FLAG_OUTPUT)
 		info->direction = GPIOD_LINE_DIRECTION_OUTPUT;